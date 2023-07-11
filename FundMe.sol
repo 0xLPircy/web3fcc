7d467994f58ef938a832341d63cac1311d530a08
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-11 13:41:05 +0530

    oracle and chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -6,11 +6,14 @@
 pragma solidity ^0.8.8;
 
 contract FundMe {
-      uint256 public number;
+    uint256 public number;
+    uint256 public minimumUsd = 50;
+    // use oracle network to find etherium in terms of usd vice versa
     function fund() public payable {
         
         // set minimum fund amount in usd
-        require(msg.value > 1e18, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
+        require(msg.value >= 1e18, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
+        // msg.value is how much they sent with the function in terms of etherium
     }
 
     // function withdraw(){}
