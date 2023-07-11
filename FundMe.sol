e5258be7d90c1a0224e43d34115eb2353a87fc3b
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-11 19:42:20 +0530

    interfaces and price feeds

--- FundMe.sol
+++ FundMe.sol
@@ -5,6 +5,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe {
     uint256 public number;
     uint256 public minimumUsd = 50;
@@ -15,6 +32,19 @@ contract FundMe {
         require(msg.value >= 1e18, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
         // msg.value is how much they sent with the function in terms of etherium
     }
+
+    function getPrice() public {
+        // interactimg with a contract outside our project so we need
+        // ABI 
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
+
+    function getVersion () public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate() public {}
 
     // function withdraw(){}
 }
