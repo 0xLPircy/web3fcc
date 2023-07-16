a41714dbc3461343f092af43984532bbcbb6fba8
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-16 17:29:03 +0530

    understanding decimals with ether and usd and msg.value better

--- FundMe.sol
+++ FundMe.sol
@@ -4,36 +4,37 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
 
-//interface AggregatorV3Interface {
- // function decimals() external view returns (uint8);
-//
-  //function description() external view returns (string memory);
-//
-  //function version() external view returns (uint256);
-//
-  //function getRoundData(
-    //uint80 _roundId
-  //) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-//
-  //function latestRoundData()
-    //external
-    //view
-    //returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-//}
+//   function description() external view returns (string memory);
+
+//   function version() external view returns (uint256);
+
+//   function getRoundData(
+//     uint80 _roundId
+//   ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+//   function latestRoundData()
+//     external
+//     view
+//     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+// }
 
 contract FundMe {
     uint256 public number;
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18;
     // use oracle network to find etherium in terms of usd vice versa
     function fund() public payable {
         
         // set minimum fund amount in usd
-        require(msg.value >= 1e18, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
+        require(getConversionRate(msg.value) >= minimumUsd, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
         // msg.value is how much they sent with the function in terms of etherium
-        // 18 decimal cause wei
+        // msg.value is 18 decimal places cause wei
+        // so gives 1000000000000000000 wei meaning 1 ether
     }
 
     function getPrice() public view returns(uint256) {
@@ -42,8 +43,11 @@ contract FundMe {
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (, int price, , ,) = priceFeed.latestRoundData();
-        //eth in terms of usd gwei format, has 8 decimal places
-        return uint256(price * 1e10); //1**10
+        //eth in terms of usd, has 8 decimal places
+        // so if returns 300000000000 means 3000.00000000
+        return uint256(price * 1e10); //1^10
+        // so now itll be 8+10 18 just as msg.value is
+        // msg.value is uint and price int so typecastong done
     }
 
     function getVersion () public view returns(uint256) {
@@ -53,7 +57,9 @@ contract FundMe {
 
     function getConversionRate(uint256 ethAmount) public view returns(uint256){
         uint256 ethPrice = getPrice();
+        // eath amount also has 18 extra zeros
         uint256 ethAmountInUsd = (ethPrice *ethAmount) / 1e18;
+        //  div by 1e18 to not have 36 zeros after multiplication
         return ethAmountInUsd;
     }
 
