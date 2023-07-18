25a9df93a9cb4d8fa6a9dcd73d305be405ce4b2f
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-18 15:54:46 +0530

    custom error

--- FundMe.sol
+++ FundMe.sol
@@ -8,6 +8,8 @@ pragma solidity ^0.8.0;
 // import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -71,7 +73,10 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == i_owner, "Sender is not owner");
+        // require(msg.sender == i_owner, "Sender is not owner");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
 }
