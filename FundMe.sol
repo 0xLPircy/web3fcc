16cac5093e983706ec3720656700bcdeeaf4d56d
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-20 22:58:52 +0530

    fallback and recvieve

--- FundMe.sol
+++ FundMe.sol
@@ -13,7 +13,7 @@ error NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public constant minimumUsd = 50 * 1e18; // use oracle network to find etherium in terms of usd vice versa
+    uint256 public constant minimumUsd = 50 * 1e18;     // use oracle network to find etherium in terms of usd vice versa
     //  constant: 841,980 -> 822,426
     // calling: 2407 -> 307
 
@@ -22,18 +22,15 @@ contract FundMe {
 
     // setting up owner of contract
     address public immutable i_owner;
-
     // 822426 -> 798955
-    constructor() {
+    constructor(){
         i_owner = msg.sender;
     }
 
     function fund() public payable {
+        
         // set minimum fund amount in usd
-        require(
-            msg.value.getConversionRate() >= minimumUsd,
-            "didnt send enough"
-        ); //1e18 == 10^18 == 1000000000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough"); //1e18 == 10^18 == 1000000000000000000
         // msg.value is how much they sent with the function in terms of etherium
         // msg.value is 18 decimal places cause wei
         // so gives 1000000000000000000 wei meaning 1 ether
@@ -41,13 +38,10 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public onlyOwner {
+    function withdraw() public onlyOwner{
+
         // for loop
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
@@ -66,17 +60,22 @@ contract FundMe {
         // require(sendSuccess, "Send Failed");
 
         // Call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call Failed");
+
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner {
         // require(msg.sender == i_owner, "Sender is not owner");
-        if (msg.sender != i_owner) {
-            revert NotOwner();
-        }
+        if (msg.sender != i_owner) { revert NotOwner();}
         _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
