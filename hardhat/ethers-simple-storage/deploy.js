21ed866735f86d3ab55794356f6c656cd858ecc9
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-29 13:05:24 +0530

    used gnache local blockchain to deploy a contract with ethersjs

--- hardhat/ethers-simple-storage/deploy.js
+++ hardhat/ethers-simple-storage/deploy.js
@@ -1,7 +1,25 @@
 const ethers = require("ethers");
+const fs = require("fs");
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider("");
+  // CONNECTING SCRIPT TO LOCAL BLOCKCHAIN
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "HTTP://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "0x4f0850cab86f4742e6ce412c86971a409f2bf66fc82135e20eca0c0cf86507a3",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying please wait ...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
