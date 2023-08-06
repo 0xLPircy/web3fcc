8ee8df17643bbf90e654237ddde2c197f3ea1b13
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-06 10:01:42 +0530

    transaction receipts

--- hardhat/ethers-simple-storage/deploy.js
+++ hardhat/ethers-simple-storage/deploy.js
@@ -8,7 +8,7 @@ async function main() {
     "HTTP://127.0.0.1:7545"
   );
   const wallet = new ethers.Wallet(
-    "0x4f0850cab86f4742e6ce412c86971a409f2bf66fc82135e20eca0c0cf86507a3",
+    "0x1bfd6c11fcec0ef3d33430704f28369f47725cff77d2b6b4327607cab014751c",
     provider
   );
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -19,7 +19,9 @@ async function main() {
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying please wait ...");
   const contract = await contractFactory.deploy();
-  console.log(contract);
+  const transactionReceipt = await contract.deployTransaction.wait(1);
+  console.log(contract.deployTransaction);
+  console.log(transactionReceipt);
 }
 
 main()
