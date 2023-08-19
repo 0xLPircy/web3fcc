0e4794efdb51757fb26d703046ca52e53b376d01
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-19 19:52:39 +0530

    interacting with contract in hardhat both throigh local and sepolia

--- hardhat/les8hardhat/scripts/deploy.js
+++ hardhat/les8hardhat/scripts/deploy.js
@@ -1,43 +1,53 @@
 // imports
-const { ethers, run, network } = require("hardhat")
+const { ethers, run, network } = require("hardhat");
 
 // async main func
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
-  console.log("Deploying contract...");
-  const simpleStorage = await SimpleStorageFactory.deploy();
-  // await simpleStorage.deployed();
-  console.log(`Deployed Contract to ${simpleStorage.target}`)
-  // checking live network or not
-  if (network.config.chainId === 11155111){
-    await simpleStorage.deploymentTransaction().wait(6);
-    console.log("6 blocks verified")
-    await verify(simpleStorage.target, []);
-  }
+    const SimpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage");
+    console.log("Deploying contract...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    // await simpleStorage.deployed();
+    console.log(`Deployed Contract to ${simpleStorage.target}`);
+    // checking live network or not
+    if (network.config.chainId === 11155111) {
+        await simpleStorage.deploymentTransaction().wait(6);
+        console.log("6 blocks verified");
+        await verify(simpleStorage.target, []);
+    }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current value is: ${currentValue}`);
+
+    // update current value
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Upadted Value is: ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {
-  // works for etherscan only
-  console.log("Verifying...")
-  try{
-  await run("verify:verify", {
-    // subtask args
-    address: contractAddress,
-    constructorArguments: args,
-  })
-  } catch (e){
-    if (e.message.toLowerCase().includes("already verified")){
-      console.log("Already Verified");
-    } else {
-      console.log(e);
+    // works for etherscan only
+    console.log("Verifying...");
+    try {
+        await run("verify:verify", {
+            // subtask args
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified");
+        } else {
+            console.log(e);
+        }
     }
-  }
 }
 
 // main
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
