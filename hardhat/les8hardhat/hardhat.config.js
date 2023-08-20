ca1bf955693029c1bc4953c4609bc0dff71e0821
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-20 09:59:20 +0530

    custom accounts task

--- hardhat/les8hardhat/hardhat.config.js
+++ hardhat/les8hardhat/hardhat.config.js
@@ -2,6 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 // require("@nomiclabs/hardhat-etherscan");
 
+task("accounts", "Prints a list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
@@ -9,16 +16,16 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    sepolia:{
-     url: SEPOLIA_RPC_URL,
-     accounts: [PRIVATE_KEY], 
-     chainId: 11155111
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
     },
-  },
-  solidity: "0.8.19",
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  }
+    solidity: "0.8.19",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
