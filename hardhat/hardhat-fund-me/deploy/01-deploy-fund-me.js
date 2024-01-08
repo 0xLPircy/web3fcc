4fcd999e1c7808b7d459ca245ab2d60166503886
L-Pircy <taveesha.agarwal@gmail.com>
2024-01-08 13:26:19 +0530

    caught up to last done, ie 7.06 half

--- hardhat/hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ hardhat/hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -22,11 +22,12 @@ const { networkConfig } = require("../helper-hardhat-config");
 // const networkConfig = helperConfig.networkConfig;
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
+  //here named and deployments are actually pulled from hre
+  const { deploy, log } = deployments; //here we are pulling deploy and log functions out of deployments
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
 
-  // if chain id x use y etc
+  // if chain id x use address y etc
   const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
 
   // mock
